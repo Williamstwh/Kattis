3d3a8b8628ce21d13fa711f014ac25510d40b245
Weehan <deantiu56@gmail.com>
2019-06-28 10:32:44 +0800

    10 kinds of people

--- 10kindsofpeople/10kindsofpeople.cpp
+++ 10kindsofpeople/10kindsofpeople.cpp
@@ -3,27 +3,68 @@
 #include <vector>
 using namespace std;
 
-vector< vector<bool> > emptyBoolMap(int r, int c) {
-  vector< vector<bool> > emptyMap(r, vector<bool>(c));
+template <class T>
+vector< vector<T> > genericGrid(int r, int c, T initValue) {
+  vector< vector<T> > emptyMap(r, vector<T>(c));
   for (int row = 0; row < r; row++) {
     for (int col = 0; col < c; col++) {
-      emptyMap[row][col] = false;
+      emptyMap[row][col] = initValue;
     }
   }
   return emptyMap;
 }
 
-int main() {
-  int r, c;
-  cin >> r >> c;
+void fill(vector< vector<int> > &map,
+          vector< vector<bool> > &hasVisited,
+          vector< vector<int> > &islands,
+          int island, int value, int rows, int cols, int row, int col) {
+  if (row < 0 || row >= rows || col < 0 || col >= cols ||
+      hasVisited[row][col] || map[row][col] != value) {
+    return;
+  }
+  hasVisited[row][col] = true;
+  islands[row][col] = island;
+  fill(map, hasVisited, islands, island, value, rows, cols, row + 1, col);
+  fill(map, hasVisited, islands, island, value, rows, cols, row - 1, col);
+  fill(map, hasVisited, islands, island, value, rows, cols, row, col + 1);
+  fill(map, hasVisited, islands, island, value, rows, cols, row, col - 1);
+}
 
-  vector< vector<int> > map(r, vector<int>(c));
+template <class T>
+void printGrid(vector< vector<T> > grid, int r, int c) {
+  for (int row = 0; row < r; row++) {
+    for (int col = 0; col < c; col++) {
+      cout << grid[row][col];
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  // Read Inputs and Declare Data Structures
+  int rows, cols;
+  cin >> rows >> cols;
+
+  vector< vector<int> > map(rows, vector<int>(cols));
+  vector< vector<bool> > hasVisited = genericGrid(rows, cols, false);
+  vector< vector<int> > islands = genericGrid(rows, cols, 0);
 
   string line;
-  for (int row = 0; row < r; row++) {
+  for (int row = 0; row < rows; row++) {
     cin >> line;
-    for (int col = 0; col < c; col++) {
+    for (int col = 0; col < cols; col++) {
       map[row][col] = line.at(col) - 48;
+    }
+  }
+
+  int island = 1;
+
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      if (!hasVisited[row][col]) {
+        fill(map, hasVisited, islands, island, map[row][col], rows, cols, row, col);
+        island++;
+      }
     }
   }
 
@@ -34,11 +75,8 @@ int main() {
   for (int t = 0; t < n; t++) {
     cin >> sr >> sc >> er >> ec;
     sr--; sc--; er--; ec--;
-    int startType = map[sr][sc];
-
-    if (canTravel(map, emptyBoolMap(r, c),
-        r, c, sr, sc, er, ec, startType)) {
-      if (startType == 1) {
+    if (islands[sr][sc] == islands[er][ec]) {
+      if (map[sr][sc] == 1) {
         cout << "decimal" << endl;
       } else {
         cout << "binary" << endl;
