1ffe7f0aba1aa5fed9c0f54fe9573907f2c71aed
Tiu Wee Han <deantiu56@gmail.com>
2020-09-30 01:20:30 +0800

    Kattis joinstrings

--- kattis/joinstrings/joinstring.cpp
+++ kattis/joinstrings/joinstring.cpp
@@ -1,39 +1,66 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+
 using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define mst(a, b) memset((a),(b),sizeof(a))
+#define mp(a, b) make_pair(a,b)
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+#define IOSBASE ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+const ll mod = 1e9 + 7;
+
+struct Node {
+    string word;
+    Node *next;
+};
+
+struct Dll {
+    Node *start;
+    Node *end;
+};
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<Dll> v;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        Node *node = new Node{ s, NULL };
+        v.push_back(Dll{ node, node });
+    }
+
+    int a, b;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> a >> b;
+        a--; b--;
+
+        v[a].end->next = v[b].start;
+        v[a].end = v[b].end;
+    }
+
+    Node* head = v[a].start;
+    while (head != NULL) {
+        cout << head->word;
+        head = head->next;
+    }
+    cout << '\n';
+    for (Dll & dll : v) {
+        free(dll.start);
+    }
+}
 
 int main() {
-  // Read N
-  int n;
-  cin >> n;
-
-  // Initialize vector storing lines
-  string words[n + 1];
-  int graph[n + 1];
-  int ends[n + 1];
-
-  // For each of the N lines
-  for (int i = 1; i <= n; i++) {
-    // Read the line
-    string line;
-    cin >> line;
-    words[i] = line;
-    graph[i] = 0;
-    ends[i] = i;
-  }
-
-  // Number of queries
-  int q = n - 1;
-  int a, b;
-  for (int i = 0; i < q; i++) {
-    cin >> a >> b;
-    graph[ends[a]] = b;
-    ends[a] = ends[b];
-  }
-
-  while (a != 0) {
-    cout << words[a];
-    a = graph[a];
-  }
-  cout << endl;
+#ifdef READ_STDIN_FROM_FILE
+    freopen("2.in", "r", stdin);
+#endif
+    IOSBASE;
+    solve();
+    return 0;
 }
