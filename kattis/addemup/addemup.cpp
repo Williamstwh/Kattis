de8e36b102ceb4a4cf83637e0762dfad13e67641
Tiu Wee Han <deantiu56@gmail.com>
2020-10-03 19:56:05 +0800

    Kattis addemup

--- kattis/addemup/addemup.cpp
+++ kattis/addemup/addemup.cpp
@@ -68,7 +68,77 @@ public:
     }
 };
 
+map<int, int> f{
+    {1, 1},
+    {2, 2},
+    {3, -1},
+    {4, -1},
+    {5, 5},
+    {6, 9},
+    {7, -1},
+    {8, 8},
+    {9, 6},
+    {0, 0},
+};
+
+int flip(int x) {
+    int res = 0;
+    while (x != 0) {
+        int d = f[x % 10];
+        if (d == -1) return -1;
+        res = 10 * res + d;
+        x /= 10;
+    }
+    return res;
+}
+
 void solve() {
+    int n, s, c, v;
+    cin >> n >> s;
+
+    unordered_map<int, vector<int>> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        v = flip(c);
+
+        if (m.find(c) != m.end()) {
+            m[c].push_back(i);
+        } else {
+            m.insert({ c, { i } });
+        }
+
+        if (v != -1) {
+            if (m.find(v) != m.end()) {
+                m[v].push_back(i);
+            } else {
+                m.insert({ v, { i } });
+            }
+        }
+    }
+
+    DE(for (auto& p : m) { cout << p.first << '\n'; })
+
+    for (const auto& p1 : m) {
+        int a = p1.first;
+        int b = s - a;
+
+        if (m.find(b) != m.end()) {
+            const auto& p2 = m[b];
+
+            for (int x : p1.second) {
+                for (int y : p2) {
+                    if (x != y) {
+                        DE(cout << a << ", " << b << '\n');
+                        cout << "YES\n";
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "NO\n";
 }
 
 int main() {
