7fb1c192c9aca21e4dd6bad7bf31e6d5b059e47f
Tiu Wee Han <deantiu56@gmail.com>
2020-10-03 19:18:48 +0800

    Kattis listgame

---
+++ kattis/listgame/listgame.cpp
@@ -0,0 +1,102 @@
+#define INPUT "2.in"
+#define DEBUG 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ENABLE_DEBUG
+#define DEBUG 0
+#endif
+#if DEBUG == 1
+#define DE(x) (x);
+#else
+#define DE(x)
+#endif
+
+#define IOSBASE ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define mp(a,b) make_pair(a,b)
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+const int INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+const ll mod = 1e9 + 7;
+
+class UFDS {
+private:
+    vector<int> p, rank, setSizes;
+    int numSets;
+public:
+    UFDS(int N) {
+        numSets = N;
+        rank.assign(N, 0);
+        p.assign(N, 0);
+        for (int i = 0; i < N; i++)
+            p[i] = i;
+        setSizes.assign(N, 1);
+    }
+    int findSet(int i) {
+        return (p[i] == i) ? i : p[i] = findSet(p[i]);
+    }
+    bool isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+    void unionSet(int i, int j) {
+        if (!isSameSet(i, j)) {
+            int x = findSet(i), y = findSet(j);
+            if (rank[x] > rank[y]) {
+                setSizes[findSet(x)] += setSizes[findSet(y)];
+                p[y] = x;
+            } else {
+                setSizes[findSet(y)] += setSizes[findSet(x)];
+                p[x] = y;
+                if (rank[x] == rank[y])
+                    rank[y]++;
+            }
+            numSets--;
+        }
+    }
+    int setSize(int i) {
+        return setSizes[findSet(i)];
+    }
+    int numDisjointSets() {
+        return numSets;
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int m = n, count = 0;
+
+    for (int i = 2, l = (int) (sqrt(n) + 4); i < l; i++) {
+        if (m == 1) {
+            break;
+        } else if (m % i == 0) {
+            DE(cout << i << '\n')
+            count++;
+            m /= i;
+            i--;
+        }
+    }
+
+    if (m != 1) {
+        count++;
+    }
+
+    cout << count << '\n';
+}
+
+int main() {
+#ifdef READ_STDIN_FROM_FILE
+    freopen(INPUT, "r", stdin);
+#endif
+    IOSBASE;
+    solve();
+    return 0;
+}
