f81580ddeb9bbd06f0b5c30dbacf282d51b1bc11
Tiu Wee Han <deantiu56@gmail.com>
2020-10-11 11:02:28 +0800

    Kattis ignore

---
+++ kattis/ignore/ignore.cpp
@@ -0,0 +1,50 @@
+#define INPUT "1.in"
+#define DEBUG 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ENABLE_DEBUG
+#define DEBUG 0
+#endif
+#if DEBUG == 1
+#define DE(x) x;
+#else
+#define DE(x)
+#endif
+
+#define IOSBASE ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define mp(a,b) make_pair(a,b)
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+const int INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+const ll mod = 1e9 + 7;
+
+const int arr[] { 0, 1, 2, 5, 9, 8, 6 }, S = 7;
+
+void solve() {
+    int t;
+    while (cin >> t) {
+        stringstream ss;
+        while (t > 0) {
+            ss << arr[t % S];
+            t /= S;
+        }
+        cout << ss.str() << '\n';
+    }
+}
+
+int main() {
+#ifdef READ_STDIN_FROM_FILE
+    freopen(INPUT, "r", stdin);
+#endif
+    IOSBASE;
+    solve();
+    return 0;
+}
