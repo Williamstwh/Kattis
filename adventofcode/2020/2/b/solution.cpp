288736885f8addbe339cee0182aecf6a8f5505a8
Tiu Wee Han <contact@tiuweehan.com>
2020-12-02 14:39:25 +0800

    Add adventofcode 2020 2

---
+++ adventofcode/2020/2/b/solution.cpp
@@ -0,0 +1,64 @@
+#define INPUT "1.in"
+#define DEBUG 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ENABLE_DEBUG
+#define DEBUG 0
+#endif
+#if DEBUG == 1
+#define DE(x) x;
+#else
+#define DE(x)
+#endif
+
+#define IOSBASE ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define mp(a,b) make_pair(a,b)
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+const int INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+const ll mod = 1e9 + 7;
+
+void solve() {
+    string s1, s2, s3;
+
+    int validCount = 0;
+    while (cin >> s1 >> s2 >> s3) {
+        stringstream ss1(s1);
+
+        string sl, sh;
+        getline(ss1, sl, '-');
+        getline(ss1, sh, '-');
+        int l = stoi(sl), h = stoi(sh);
+
+        char curr = s2[0];
+
+        int count = 0;
+        for (char c : s3) {
+            if (c == curr) {
+                count++;
+            };
+        }
+
+        if (l <= count && count <= h) {
+            validCount++;
+        }
+    }
+    cout << validCount << '\n';
+}
+
+int main() {
+#ifdef READ_STDIN_FROM_FILE
+    freopen(INPUT, "r", stdin);
+#endif
+    IOSBASE;
+    solve();
+    return 0;
+}
