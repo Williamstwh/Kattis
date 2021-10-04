b3a50dcc1c5a5ad470ad8b27294fa58d2d69db45
Tiu Wee Han <deantiu56@gmail.com>
2021-10-04 10:16:22 +0800

    Kattis apaxiaaans

---
+++ kattis/apaxiaaans/apaxiaaans.cpp
@@ -0,0 +1,52 @@
+#define INPUT "1.in"
+#define DEBUG 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ENABLE_DEBUG
+#define DEBUG 0
+#endif
+#if DEBUG == 1
+#define DE(x) x;
+#else
+#define DE(x)
+#endif
+
+#define IOSBASE ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define umap unordered_map
+#define uset unordered_set
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define mp(a,b) make_pair(a,b)
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+const int INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+const ll mod = 1e9 + 7;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    stringstream ss;
+    ss << s[0];
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] != s[i-1]) {
+            ss << s[i];
+        }
+    }
+    cout << ss.str();
+}
+
+int main() {
+#ifdef READ_STDIN_FROM_FILE
+    freopen(INPUT, "r", stdin);
+#endif
+    IOSBASE;
+    solve();
+    return 0;
+}
